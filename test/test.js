66dee7776718f3a47b1a91e958adf8b932650c95
David Karapetyan <davitkrpt@gmail.com>
2020-03-09 23:55:35 +0300

    check for identical stages before appending a stage

--- test/test.js
+++ test/test.js
@@ -634,6 +634,49 @@ describe('Aggregate features', () => {
       })
         .catch((err) => done(err));
     });
+
+    it('Two nested relations with overlapping paths shouldn\'t overwrite each other', (done) => {
+      const countryName = 'Great Britain';
+      const cityName = 'London';
+      Person.aggregate({
+        include: [{
+          relation: 'company',
+          scope: {
+            include: [
+              {
+                relation: 'city',
+                scope: {
+                  include: ['country'],
+                },
+              },
+            ],
+          },
+        }],
+        where: {
+          'company.city.country.name': countryName,
+          'company.city.name': cityName,
+        },
+        limit: 1,
+      }).then((people) => {
+        should.exist(people);
+        people.should.be.Array().and.length(1);
+        const person = people[0];
+        const company = person.company();
+        company.should.be.Object();
+        company.should.have.property('city');
+
+        const city = company.city();
+        city.should.be.Object();
+        city.should.have.property('name').which.is.eql(cityName);
+        city.should.have.property('country');
+
+        const country = city.country();
+        country.should.be.Object();
+        country.should.have.property('name').which.is.eql(countryName);
+        done();
+      })
+        .catch((err) => done(err));
+    });
   });
 
 });
